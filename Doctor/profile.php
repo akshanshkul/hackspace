04a34906ea8dc31cac650d4577a9f362ed13d2f8
AbhayGupta09 <abhay20gupta@gmail.com>
2024-02-17 13:33:45 +0530

    profile page added

---
+++ Doctor/profile.php
@@ -0,0 +1,31 @@
+<?php
+require 'header.php';
+?>
+
+<div class="container-fluid">
+    <div class="card" style="width:500px">
+        <div class="card-body">
+            <div class="mb-4"><img src="../assets/images/profile/user-1.jpg"
+                    class="card-img-top profile-img img-thumbnail" alt="Profile Picture" style="width:50%;">
+            </div>
+            <h5 class="card-title">John Doe</h5>
+            <p class="card-text">mob: 1234567890</p>
+            <p class="card-text">ad : jo bhi hai </p>
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Department : Optometry</li>
+                <li class="list-group-item">Post : Optometry</li>
+                <li class="list-group-item">Experience : 10 yrs</li>
+                <li class="list-group-item">Email: john.doe@example.com</li>
+                <li class="list-group-item">Location: New York, USA</li>
+            </ul>
+            <!-- <div class="card-body">
+        <a href="#" class="btn btn-info me-md-3">View Profile </a>
+        <a href="#" class="btn btn-info ">Edit Profile</a>
+      </div> -->
+        </div>
+    </div>
+</div>
+<?php
+require 'footer.php';
+?>
