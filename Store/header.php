efec883ea26b95e6922243b6ee7aa5b63748dd6b
Akpk1234 <akashsh888@gmail.com>
2024-02-17 14:51:51 +0530

    hyhyji

--- Store/header.php
+++ Store/header.php
@@ -9,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Medipure</title>
   <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" />
-  <link rel="stylesheet" href="assets/css/styles.min.css" />
+  <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
 
 <body>
